a709eb0f17f58bf047a9d8222cf75e38a3f19644
Lonik <l0nik@mail.ru>
2020-03-22 08:46:08 +0300

    put the code to the solution1() procedure

--- problem1/problem1.py
+++ problem1/problem1.py
@@ -1,6 +1,10 @@
-num = 1000
-sum = 0
-for i in range(1, num):
-    if i%3 == 0 or i%5 == 0:
-        sum += i
-print(sum)
+
+def solution1():
+    num = 1000
+    sum = 0
+    for i in range(1, num):
+        if i%3 == 0 or i%5 == 0:
+            sum += i
+    print(sum)
+
+solution1()
